cb03f70b5a183aaa0cb1360df713c52aed967379
Eberson Silva <binho.ebs@gmail.com>
2023-03-25 21:41:28 -0300

    Criação da perte visual do projeto

---
+++ node_modules/@ignite-ui/react/src/styles/index.ts
@@ -0,0 +1,9 @@
+import { colors, fontSizes } from "@ignite-ui/tokens";
+import { createStitches } from "@stitches/react";
+
+createStitches({
+  theme:{
+    colors: colors,
+    fontSizes: fontSizes
+  }
+})
