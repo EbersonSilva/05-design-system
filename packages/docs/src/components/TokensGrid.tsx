772b381fde4356d03935adaade54e24c2873c81d
Eberson Silva <binho.ebs@gmail.com>
2023-04-20 23:01:11 -0300

    Atualização do projeto

--- packages/docs/src/components/TokensGrid.tsx
+++ packages/docs/src/components/TokensGrid.tsx
@@ -1,11 +1,26 @@
-export function TokensGrid(){
-    return(
-        <table>
-            <thead>
-                <tr>
-                    <th></th>
-                </tr>
-            </thead>
-        </table>
-    )
+interface TokensGridProps {
+  tokens: Record<string, string>
+}
+
+export function TokensGrid({ tokens }: TokensGridProps) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Value</th>
+        </tr>
+      </thead>
+      <tbody>
+        {Object.entries(tokens).map(([key, value]) => {
+          return (
+            <tr key={key}>
+              <td>{key}</td>
+              <td>{value}</td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
 }
